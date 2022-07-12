47ca02eb1db19472598b16cd1088481006dbd6e0
AndriejUa <109022360+AndriejUa@users.noreply.github.com>
2022-07-12 19:19:02 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,37 @@
+const mineflayer = require('mineflayer');
+
+const botArgs = {
+    host: 'game7.falixserver.net',
+    port: '10844',
+    username: "SbyWorld",
+    version: '1.17.1'
+};
+
+const initBot = () => {
+
+    // Setup bot connection
+    let bot = mineflayer.createBot(botArgs);
+
+    bot.on('login', () => {
+        let botSocket = bot._client.socket;
+        console.log(`Logged in to ${botSocket.server ? botSocket.server : botSocket._host}`);
+    });
+
+    bot.on('end', () => {
+        console.log(`Disconnected`);
+
+        // Attempt to reconnect
+        setTimeout(initBot, 5000);
+    });
+
+    bot.on('error', (err) => {
+        if (err.code === 'ECONNREFUSED') {
+            console.log(`Failed to connect to ${err.address}:${err.port}`)
+        }
+        else {
+            console.log(`Unhandled error: ${err}`);
+        }
+    });
+};
+
+initBot();
